8c614d4a912c42e896058d72fe093ab868c2aad5
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-02-10 16:23:39 +0100

    Fix the router when Next.js returns no result

--- crates/next-core/src/router.rs
+++ crates/next-core/src/router.rs
@@ -93,6 +93,7 @@ enum RouterIncomingMessage {
     FullMiddleware {
         data: FullMiddlewareResponse,
     },
+    None,
     Error(StructuredError),
 }
 
@@ -101,6 +102,7 @@ enum RouterIncomingMessage {
 pub enum RouterResult {
     Rewrite(RewriteResponse),
     FullMiddleware(FullMiddlewareResponse),
+    None,
     Error,
 }
 
@@ -109,6 +111,7 @@ impl From<RouterIncomingMessage> for RouterResult {
         match value {
             RouterIncomingMessage::Rewrite { data } => Self::Rewrite(data),
             RouterIncomingMessage::FullMiddleware { data } => Self::FullMiddleware(data),
+            RouterIncomingMessage::None => Self::None,
             _ => Self::Error,
         }
     }
